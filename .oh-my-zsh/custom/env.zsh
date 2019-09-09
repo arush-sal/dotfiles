836df86f0d26c4620819bed72169f87cea463f2e
Arush Salil <me@aru.sh>
2019-09-09 15:10:02 +0530

    clarify further the brokenness of C++. why the fuck are we using C++?
    Signed-off-by: Arush Salil <me@aru.sh>

--- .oh-my-zsh/custom/env.zsh
+++ .oh-my-zsh/custom/env.zsh
@@ -3,6 +3,6 @@
  export GOSRC="$GOPATH/src"
  export GOGITHUB="$GOSRC/github.com"
  export GOARUSH="$GOGITHUB/arush-sal"
- export GO111MODULE=on
+ export GO111MODULE=off
  export DOCKER_BUILDKIT=1
  export PATH="$PATH:$GOBIN:$HOME/.local/bin:${KREW_ROOT:-$HOME/.krew}/bin"
