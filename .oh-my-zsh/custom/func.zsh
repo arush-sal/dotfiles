f6b8fee6f52444d50531372e1819eba58b508caa
Arush Salil <me@aru.sh>
2020-05-27 15:10:05 +0530

    For the sake of my sanity, just ignore this...
    Signed-off-by: Arush Salil <me@aru.sh>

--- .oh-my-zsh/custom/func.zsh
+++ .oh-my-zsh/custom/func.zsh
@@ -41,3 +41,9 @@ kind_update() {
 	sudo mv $download_location $bin_location
 	sudo chmod +x $bin_location
 }
+
+kubectl_update() {
+	curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+	chmod +x ./kubectl
+	sudo mv ./kubectl /usr/local/bin/kubectl
+}
