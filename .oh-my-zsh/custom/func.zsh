a2c90440ba3af46016710a13f6011ad5c7346033
Arush Salil <me@aru.sh>
2022-01-31 15:00:03 +0530

    Whee, good night.
    Signed-off-by: Arush Salil <me@aru.sh>

--- .oh-my-zsh/custom/func.zsh
+++ .oh-my-zsh/custom/func.zsh
@@ -60,3 +60,10 @@ safari-dl() {
 			--output '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' $@
 	fi
 }
+
+gh-cloc() {
+  git clone --depth 1 "$1" temp-linecount-repo &&
+  printf "('temp-linecount-repo' will be deleted automatically)\n\n\n" &&
+  cloc temp-linecount-repo &&
+  rm -rf temp-linecount-repo
+}
